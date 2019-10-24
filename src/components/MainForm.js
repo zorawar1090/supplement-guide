443769bd2cbc6c20a8bfc17845f0a4b571a42e8c
zorawar1090 <zorawarsingh9988@gmail.com>
2019-10-25 00:24:38 +0200

    add Main Form

---
+++ src/components/MainForm.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import GenderForm from 'GenderForm'
+import LevelForm from 'LevelForm'
+import GoalForm from 'GoalForm'
+
+export default class MainForm extends React.Component {
+  state = {
+    step: 1,
+    gender: "",
+    level: "",
+    goal: ""
+  }
+
+  nextStep = () => {
+    const { step } = this.state
+    this.setState(
+      {
+        step: step + 1
+      }
+    )
+  }
+
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state
+    const { gender, level, goal } = this.state
+    const values = { gender, level, goal }
+
+    switch (step) {
+      case 1:
+        return (
+          <GenderForm
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        )
+      case 2:
+        return (
+          <LevelForm
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        )
+      case 3:
+        return (
+          <GoalForm
+            values={values}
+          />
+        )
+      default: 
+          return
+    }
+  }
+}
+
+
+
